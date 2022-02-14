76d3c348bf2095dd1c36ccaecaf97e270302c461
victoriamarvie <88499364+victoriamarvie@users.noreply.github.com>
2022-02-13 21:38:04 -0300

    Add files via upload

---
+++ portfolio projeto/dist/js.js
@@ -0,0 +1,27 @@
+$('.horizontal-scroll').horizontalScroll({
+
+	 
+	
+    // container height
+  
+    "containerHeight":"300vh",
+  
+  
+    // image height
+  
+    "itemsHeight": base.maxHeight,
+  
+  
+    // slider width
+  
+    "width": base.sumWidths.toString() +"px",
+  
+    // top padding
+  
+    "paddingTop": base.paddingTop,
+  
+    // left padding
+  
+    "paddingLeft": base.paddingLeft,
+     
+  });
